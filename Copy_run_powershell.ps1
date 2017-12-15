e45b456954873147c497fa6584f8c7edbdb6cdb5
Vas Kuruganti <pk.itconsultant@outlook.com>
2017-12-15 15:38:36 -0500

    Create Copy_run_powershell.ps1

---
+++ Copy_run_powershell.ps1
@@ -0,0 +1,15 @@
+#$filePath =  "C:\Users\Desktop\t1.txt"   
+$machines = Get-Content C:\Users\Desktop\t1.txt
+
+foreach($machine in $machines)
+{
+    $job = {
+    Copy-Item -Path \\common\auto_install -Destination \\$machine\C$\
+
+    Invoke-Expression C:\NBU_auto_install  #Invoke-Command -ComputerName $machine -ScriptBlock {Get-ChildItem "C:\NBU_auto_install"} -ErrorAction Stop
+}
+
+# Invoke-Command -ComputerName $machine -ScriptBlock $job -ArgumentList $machine -ErrorAction SilentlyContinue
+    
+
+}
